24feda778b6f14503fa8a8777b0a59ab7440de45
Alexey Pargachev <pargachev20@gmail.com>
2021-11-05 00:36:16 +0700

    init

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Book struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var books []Book
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func main() {
+
+}
